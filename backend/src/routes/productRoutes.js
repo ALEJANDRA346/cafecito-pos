3b2e38256e4037b91600a83abd1bcc38d35a82ae
ALEJANDRA346 <aleulloadls@gmail.com>
2026-01-23 14:58:18 -0600

    AuthMiddleware

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Rutas Públicas (o protegidas, según queramos luego)
 router.get('/', productController.getProducts);
 
 // Rutas de Admin
-router.post('/', productController.createProduct);      // Crear
-router.put('/:id', productController.updateProduct);    // Editar (necesita ID)
-router.delete('/:id', productController.deleteProduct); // Borrar (necesita ID)
+router.post('/', authMiddleware, productController.createProduct);
+router.put('/:id', authMiddleware, productController.updateProduct);
+router.delete('/:id', authMiddleware, productController.deleteProduct);
 
 module.exports = router;
